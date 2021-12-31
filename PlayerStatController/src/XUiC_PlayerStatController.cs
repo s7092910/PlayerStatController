590aaea1777f843f76e58f9d63d2ee1c0a0abc2e
Chris <chris.beda@gmail.com>
2021-12-30 21:31:44 -0800

    Fixing the weird folder renaming part 2

---
+++ PlayerStatController/src/XUiC_PlayerStatController.cs
@@ -0,0 +1,145 @@
+﻿/*Copyright 2021 Christopher Beda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+using System.Collections.Generic;
+
+public class XUiC_PlayerStats : XUiController
+{
+    private string logTag = typeof(XUiC_PlayerStats).ToString();
+
+    public bool isDirty;
+
+	private EntityPlayer localPlayer;
+    private List<BindingType> activeBindings = new List<BindingType>();
+    private Dictionary<BindingType, string> bindingsLastValue = new Dictionary<BindingType, string>();
+    public override void Init()
+	{
+		base.Init();
+		this.isDirty = true;
+	}
+
+    /// <summary>
+    /// Determines if the values have changed related to the bindings for this controller. If the values<br/>
+    /// have changed or if the UI is dirty, it will give a call to get the new values for each binding.
+    /// </summary>
+	public override void Update(float _dt)
+	{
+		base.Update(_dt);
+		if (localPlayer == null && XUi.IsGameRunning())
+		{
+			localPlayer = base.xui.playerUI.entityPlayer;
+		}
+		if (isDirty || HasChanged())
+		{
+            Logging.Out(logTag, "isDirty = " + isDirty + ", localPlayer = " + localPlayer);
+            if (localPlayer == null)
+			{
+				return;
+			}
+			isDirty = false;
+			base.RefreshBindings(false);
+		}
+	}
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+        isDirty = true;
+    }
+
+    public override void OnClose()
+    {
+        base.OnClose();
+    }
+
+    /// <summary>
+    /// Gets the value related to the BindingName.
+    /// </summary>
+    /// <param name="value">The returned string that contains the value to be used in the xml for the binding</param>
+    /// <param name="bindingName">The bindingName in the xml</param>
+    /// <returns><see langword="true"/> if the value has been found or has a <see cref="BindingType">, otherwise <see langword="false"/></returns>
+    public override bool GetBindingValue(ref string value, string bindingName)
+    {
+        Logging.Out(logTag, "GetBindingValue(), bindingName = " + bindingName);
+        if (bindingName != null)
+        {
+            if (localPlayer != null)
+            {
+                value = GetBindingValue(bindingName);
+                Logging.Out(logTag, "GetBindingValue(), bindingValue = " + value);
+            }
+            else
+            {
+                value = "";
+            }
+
+            //Checks to see if value is empty or null. If it is, it will check to see if there is a supported Binding
+            //for the bindingName, it will return false if no BindingType is found.
+            if (string.IsNullOrEmpty(value))
+            {
+                return BindingType.SupportsBinding(bindingName);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Determines if any of the active <see cref="BindingType"> has their binding value updated.
+    /// </summary>
+    /// <returns><see langword="true"/> if any binding value has been updated</returns>
+    private bool HasChanged()
+    {
+        foreach(BindingType stat in activeBindings)
+        {
+            string lastValue = bindingsLastValue[stat];
+            if (stat.HasValueChanged(localPlayer, ref lastValue)) 
+            {
+                bindingsLastValue[stat] = lastValue;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Searches the activeBindings for the corresponding <see cref="BindingType"/>. If the <see cref="BindingType"/> is not found <br/>
+    /// in the activeBindings, searches the Supported <see cref="BindingType">BindingTypes</see> by looking up the bindingName.
+    /// <para>
+    /// If a <see cref="BindingType"/> has been found, it will fetch the current binding value
+    /// </para>
+    /// </summary>
+    /// <param name="bindingName">the bindingName in the xml and the name of the <see cref="BindingType"/></param>
+    /// <returns>The value for the binding</returns>
+    private string GetBindingValue(string bindingName)
+    {
+        BindingType stat = activeBindings.Find(f => f.Name == bindingName);
+        if (stat == null)
+        {
+            stat = BindingType.FromBindingName(bindingName);
+            if (stat == null)
+            {
+                return "";
+            }
+            activeBindings.Add(stat);
+            bindingsLastValue.Add(stat, "");
+        }
+
+        return stat.GetCurrentValue(localPlayer);
+    }
+}
+
---
+++ PlayerStatController/src/binding/Enumeration.cs
@@ -0,0 +1,76 @@
+﻿/*Copyright 2021 Christopher Beda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+using System;
+
+/// <summary>
+/// Base clase for BindingType so BindingType has enum like characteristics
+/// </summary>
+public abstract class Enumeration : IComparable
+{
+    private readonly int value;
+    private readonly string name;
+
+    protected Enumeration(int value, string name)
+    {
+        this.value = value;
+        this.name = name;
+    }
+
+    public int Value
+    {
+        get { return value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        var otherValue = obj as Enumeration;
+
+        if (otherValue == null)
+        {
+            return false;
+        }
+
+        var typeMatches = GetType().Equals(obj.GetType());
+        var valueMatches = value.Equals(otherValue.Value);
+
+        return typeMatches && valueMatches;
+    }
+
+    public override int GetHashCode()
+    {
+        return value.GetHashCode();
+    }
+
+    protected static int AbsoluteDifference(Enumeration firstValue, Enumeration secondValue)
+    {
+        var absoluteDifference = Math.Abs(firstValue.Value - secondValue.Value);
+        return absoluteDifference;
+    }
+
+    public int CompareTo(object other)
+    {
+        return Value.CompareTo(((Enumeration)other).Value);
+    }
+}
